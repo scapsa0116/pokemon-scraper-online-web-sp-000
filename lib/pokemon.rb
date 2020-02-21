4317fa28e0ada44a7dfa2482a572cf3bf0f50049
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 18:48:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_reader :id, :name, :type, :db
    
   
-  def initialize(id = nil, name, type, db)
+  def initialize(id, name, type, db)
     @id = id 
     @name = name 
     @type = type 
