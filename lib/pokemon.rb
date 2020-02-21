0ab2dc3761a3ed572447ac0ed3a113f6a110b509
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 19:06:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,11 +13,5 @@ class Pokemon
   def self.save(name, type, database_connection)
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
-def self.find(id)
-    pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    name = pokemon[1]
-    type = pokemon[2]
-    hp = pokemon[3]
-  end 
- 
+
 end
