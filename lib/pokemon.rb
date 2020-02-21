dc85fe9cb19fc314d223ed682c94429fd27f82c0
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 19:02:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,17 +14,5 @@ class Pokemon
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def self.find(id, database_connection)
-    pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    name = pokemon[1]
-    type = pokemon[2]
-  
-
-    pokemon_inst = Pokemon.new(id: id, name: name, type: type, db: database_connection)
-  end
-
-  def alter_hp( database_connection)
-    database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", @id)
-  end
-
+ 
 end
