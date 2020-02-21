81020d0cfb718d4435eacb499bd4c2056a1a88b8
Stela Capsa <stela.capsa@gmail.com>
2020-02-21 19:03:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,6 +13,8 @@ class Pokemon
   def self.save(name, type, database_connection)
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
-
+def self.find(id) 
+  
+end 
  
 end
